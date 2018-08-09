ce79697e819ed502ae34f18e370f3f97fa1183eb
Thom David <thomdavid74@gmail.com>
2018-08-09 15:38:31 -0700

    L33 Vue render function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  // h is Vue shorthand for Vue's createElement function
+  // code below is equivalent to
+  //  render: function(createElement) {
+  //  return createElement(App)
+  //  }
+  render: h => h(App)
+  
+})
