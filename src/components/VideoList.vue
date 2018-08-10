123a8b4bf93f2859459384e8e5d4ea75e88c80be
Thom David <thomdavid74@gmail.com>
2018-08-10 14:56:32 -0700

    L52 VideoListItem component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -6,8 +6,14 @@
 </template>
 
 <script>
+
+import VideoListItem from './VideoListItem' // import this component
+
 export default {
   name: 'VideoList',
+  components: {
+    VideoListItem // register imported components
+  },
   props: {  // properties to be passed from parent
     videos: Array  // property validation, type of data to be received from parent
   } 
