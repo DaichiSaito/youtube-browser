24aee67b640ae63de5eb41075766c5b5991ba6c8
Thom David <thomdavid74@gmail.com>
2018-08-10 14:43:34 -0700

    L50 VideoList prop configuration

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,16 @@
 <template>
   <ul>
     VideoList
+    {{ videos.length }}
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'VideoList'
+  name: 'VideoList',
+  props: {  // properties to be passed from parent
+    videos: Array  // property validation, type of data to be received from parent
+  } 
 }
 </script>
 
