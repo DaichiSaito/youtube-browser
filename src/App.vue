c7369bf5c061c36dc6c4e0ed2e8a2406461d2ee6
Thom David <thomdavid74@gmail.com>
2018-08-10 13:18:18 -0700

    L49 Acquire list of videos test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <SearchBar @termChange="onTermChange"></SearchBar>
     <VideoList></VideoList>
+    {{ videos.length }}
   </div>
 </template>
 
@@ -19,6 +20,11 @@ export default {
     SearchBar,
     VideoList
   },
+  data() { // in a component, the data property must be a function that returns and object
+    return {
+      videos: []
+    }
+  },
   methods: {
     onTermChange(searchTerm) {
       axios.get('https://www.googleapis.com/youtube/v3/search', {
@@ -28,8 +34,11 @@ export default {
           part: 'snippet', // ask for a small portion of each video returned in search
           q: searchTerm // q = query, which will be the searchTerm passed to the method
         }
-      }) // returns a promise
-      .then(response => console.log(response))
+      }) // returns a promise, so
+      .then(response => {
+        this.videos = response.data.items //data property of RESPONSE, items property of data
+        // console.log(this.videos)
+      })
     }
   }
 }
