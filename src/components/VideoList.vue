c1c8c3a5f98d178f797d630d6090a14ef6d899b4
Thom David <thomdavid74@gmail.com>
2018-08-10 17:34:32 -0700

    Code comments

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -10,13 +10,16 @@
 </template>
 
 <script>
-
+// In the template v-for directive, video refers to each item in the videos array
+// That individual item then becomes a prop passed from the parent (this component)
+//  to the child component (VideoListItem)
+// video is added to child component as a prop it should expect to receive from parent
 import VideoListItem from './VideoListItem' // import this component
 
 export default {
   name: 'VideoList',
   components: {
-    VideoListItem // register imported components
+    VideoListItem // register imported (child) components
   },
   props: {  // properties to be passed from parent
     videos: Array  // property validation, type of data to be received from parent
--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
-
+// props:video below is the prop to be received from parent component
+//  (VideoList)
+// snippet.title (above in template) is a property of the indiv video
 export default {
   name: 'VideoListItem',
   props: ['video']
