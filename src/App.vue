01a74f2b91f7c598ccee506a46086b6e7df715ff
Thom David <thomdavid74@gmail.com>
2018-08-11 13:16:29 -0700

    L65-66 VideoDetail wired

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoDetail :video="selectedVideo" />
     <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
   </div>
 </template>
@@ -9,6 +10,7 @@
 import axios from 'axios' // library for processing search
 import SearchBar from './components/SearchBar' // Make SearchBar component available to template
 import VideoList from './components/VideoList' // Make VideoList component available
+import VideoDetail from './components/VideoDetail' // Make VideoDetail component available
 
 
 const API_KEY = 'API_KEY_HERE'
@@ -17,16 +19,18 @@ export default {
   name: 'App',
   components: { // register any components being used
     SearchBar,
-    VideoList
+    VideoList,
+    VideoDetail
   },
   data() { // in a component, the data property must be a FUNCTION that returns an object
     return {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
   },
   methods: {
     onVideoSelect(video) {
-      console.log(video)
+      this.selectedVideo = video
     },
     onTermChange(searchTerm) {
       axios.get('https://www.googleapis.com/youtube/v3/search', {
