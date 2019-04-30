1d6af7a7b0c13152930ba05a6c751bf090bb75e3
DaichiSaito <hrkedz@gmail.com>
2019-04-30 20:19:44 +0900

    youtubeのapiキーを環境変数に書いた。

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,57 @@
   <div id="app" class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
     <div class="row">
-      <VideoDetail :video="selectedVideo" />
+      <VideoDetail :video="selectedVideo"/>
       <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios' // library for processing search
-import SearchBar from './components/SearchBar' // Make SearchBar component available to template
-import VideoList from './components/VideoList' // Make VideoList component available
-import VideoDetail from './components/VideoDetail' // Make VideoDetail component available
+import axios from "axios"; // library for processing search
+import SearchBar from "./components/SearchBar"; // Make SearchBar component available to template
+import VideoList from "./components/VideoList"; // Make VideoList component available
+import VideoDetail from "./components/VideoDetail"; // Make VideoDetail component available
 
-
-const API_KEY = 'API_KEY_HERE'
+const API_KEY = process.env.VUE_APP_API_KEY;
 
 export default {
-  name: 'App',
-  components: { // register any components being used
+  name: "App",
+  components: {
+    // register any components being used
     SearchBar,
     VideoList,
     VideoDetail
   },
-  data() { // in a component, the data property must be a FUNCTION that returns an object
+  data() {
+    // in a component, the data property must be a FUNCTION that returns an object
     return {
       videos: [],
       selectedVideo: null
-    }
+    };
   },
   methods: {
     onVideoSelect(video) {
-      this.selectedVideo = video
+      this.selectedVideo = video;
     },
     onTermChange(searchTerm) {
-      axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          type: 'video', // type of data to retrieve
-          part: 'snippet', // ask for a small portion of each video returned in search
-          q: searchTerm // q = query, which will be the searchTerm passed to the method
-        }
-      }) // returns a promise, so
-      .then(response => {
-        this.videos = response.data.items //data property of RESPONSE, items property of data
-        // console.log(this.videos)
-      })
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video", // type of data to retrieve
+            part: "snippet", // ask for a small portion of each video returned in search
+            q: searchTerm // q = query, which will be the searchTerm passed to the method
+          }
+        }) // returns a promise, so
+        .then(response => {
+          this.videos = response.data.items; //data property of RESPONSE, items property of data
+          // console.log(this.videos)
+        });
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
